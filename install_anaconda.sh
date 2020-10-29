08f52243e519468f5dcc14a36876d868186d496c
Harimus <dan.lillrank@gmail.com>
2020-10-29 19:17:20 +0900

    fixed typo in script

--- install_anaconda.sh
+++ install_anaconda.sh
@@ -7,12 +7,14 @@ curl -O https://repo.anaconda.com/archive/Anaconda3-2020.07-Linux-x86_64.sh
 
 CORRECT_SHA=38ce717758b95b3bd0b1797cc6ccfb76f29a90c25bdfa50ee45f11e583edfdbf
 
-echo "${CORRECT_SHA} Anaconda3-2020.07-Linux-x86_64.sh" | sha256sum -C
+echo "${CORRECT_SHA} Anaconda3-2020.07-Linux-x86_64.sh" | sha256sum -c
 bash ./Anaconda3-2020.07-Linux-x86_64.sh
 
 rm ./Anaconda3-2020.07-Linux-x86_64.sh
 echo "If you continue the installation on this terminal, dont forget to type the command: source ~/.bashrc"
 cd ${CURRENT_DIRECTORY}
+source ~/.bashrc
+conda config --set auto_activate_base false
 
 
 
--- install_sc2.sh
+++ install_sc2.sh
@@ -5,7 +5,10 @@ cd /tmp
 
 curl -O http://blzdistsc2-a.akamaihd.net/Linux/SC2.4.10.zip
 CORRECT_SHA=1052f249f30a242bf5e17cc3315324919f9689307ca6dbcdb30bb41ea3004884
-echo "${CORRECT_SHA} SC2.4.10.zip" | sha256sum -C
+echo "${CORRECT_SHA} SC2.4.10.zip" | sha256sum -c
 sudo apt-get install unzip
 unzip -P iagreetotheeula SC2.4.10.zip -d ~/
 rm SC2.4.10.zip
+
+echo "download complete. test your installation with: python -m pysc2.bin.agent --map Simple64"
+echo "Note: (dont forget to conda activate your environment)"
